9d847dd70eab46668a6e1bd31760c1a5d223aeb9
정성규 <jsg213213@naver.com>
2026-05-15 11:40:01 +0845

    first commit

---
+++ src/main/java/com/busanit401/geminitestproject/GeminiTestprojectApplication.java
@@ -0,0 +1,13 @@
+package com.busanit401.geminitestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiTestprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeminiTestprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/busanit401/geminitestproject/GeminiTestprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busanit401.geminitestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiTestprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
